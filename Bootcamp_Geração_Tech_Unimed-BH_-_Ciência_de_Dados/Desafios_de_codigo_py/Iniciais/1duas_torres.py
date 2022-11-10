9cc9bef53d304bcecb2a65c5215273fbec2bfed8
Joadir Damasceno Junior <115026650+TMKaT@users.noreply.github.com>
2022-11-10 10:53:51 -0300

    Create 1duas_torres.py

---
+++ Bootcamp_Geração_Tech_Unimed-BH_-_Ciência_de_Dados/Desafios_de_codigo_py/Iniciais/1duas_torres.py
@@ -0,0 +1,43 @@
+'''
+====================================
+DESAFIO BÁSICO 1/3 - As Duas Torres
+====================================
+~~~~ENUNCIADO:
+Saruman, o Branco, um grande mago da Terra-média, traiu os bons costumes e se filiou ao lorde do mal, Sauron. Sauron comanda a torre de Minas Morgul, que abriga um dos seus mais temidos servos, o Rei Bruxo de Angmar, um dos Nazgûl (antigos reis humanos que foram corrompidos pelos poderes dos anéis de Sauron). Saruman comanda a torre de Orthanc, onde cria seus servos Uruk-hai, orcs mais terríveis que os convencionais. Para comunicação, eles utilizam as relíquias esféricas chamadas Palantír, que ficam no topo de suas torres.
+A Terra-média avança cada vez mais em tecnologia, muito impulsionada pelas guerras que a acometem diariamente. Um dos problemas que tem atrapalhado sua população é a Interferência de Comunicação Mágica (ICM). Os estudiosos de Minas Tirith, grande cidadela de Gondor, concluíram que para calcular o ICM para Palantír’s, basta dividir a distância entre os dois Palantír’s, pela soma do diâmetro dos mesmos. Gandalf, o Cinza, chegou a questionar essa conclusão, alegando que ela não fazia muito sentido, mas ele mesmo concluiu que dar sentido às coisas não faz sentido.
+Saruman e Sauron precisam de uma comunicação estável, pois têm medo que Frodo e seus amigos consigam atrapalhar seus planos, portanto, querem saber quanto de ICM há na comunicação de seus Palantír’s, para que saibam quanto de magia devem empregar na comunicação.
+
+~~~~ENTRADA:
+A entrada é composta por 3 inteiros, N(0 < N < 10000), X e Y(0 < X, Y < 100), que indicam, respectivamente, a distância entre os Palantír, o diâmetro do Palantír de Sauron e o diâmetro do Palantír de Saruman.
+
+~~~~SAÍDA:
+Um valor real indicando o ICM da comunicação dos Palatír de Sauron e Saruman, com 2 casas decimais.
+
+--------------------------------------
+Exemplo de Entrada | Exemplo de Saída
+--------------------------------------
+100 2 2            | 25.00
+--------------------------------------
+200 3 8            | 18.18
+--------------------------------------
+'''
+
+# DICAS SOBRE PYTHON:
+# FUNÇÃO input(): Ela recebe como parâmetro uma String que será visível ao usuário, 
+# onde geralmente informa que tipo de informação ele está esperando receber;
+# FUNÇÃO print(): Ela é a responsável por imprimir os dados e informar os valores no terminal;
+# MÉTODO split(): permite dividir o conteúdo da variável de acordo com as condições especificadas 
+# em cada parâmetro da função ou com os valores predefinidos por padrão;
+
+# Abaixo segue um exemplo de código que você pode ou não utilizar
+entrada = input().split(" ")
+
+distancia = int(entrada[0])
+diametro1 = int(entrada[1])
+diametro2 = int(entrada[2])
+
+# TODO: Calcule o ICM da comunicação dos Palatír de Sauron e Saruman, com 2 casas decimais no espaço #em branco abaixo:
+resultado = distancia/(diametro1+diametro2)
+
+if (0 < distancia < 1000) and (0 < diametro1, diametro2 <100):
+  print(f'{resultado:.2f}')
