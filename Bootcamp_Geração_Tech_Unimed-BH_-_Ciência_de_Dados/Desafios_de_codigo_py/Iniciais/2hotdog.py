4aad5081a9801ec9bca187ce80dd774987e84570
Joadir Damasceno Junior <115026650+TMKaT@users.noreply.github.com>
2022-11-10 10:54:42 -0300

    Create 2hotdog.py

---
+++ Bootcamp_Geração_Tech_Unimed-BH_-_Ciência_de_Dados/Desafios_de_codigo_py/Iniciais/2hotdog.py
@@ -0,0 +1,46 @@
+'''
+=====================================
+DESAFIO BÁSICO 2/3 - Cachorro Quente
+=====================================
+~~~~ENUNCIADO:
+Em 2012 foi alcançado um novo recorde mundial na famosa Competição de Cachorros-Quentes do Nathan: o campeão, Joey Chestnut, devorou 68 cachorros-quentes em dez minutos, um aumento incrível em relação aos 62 sanduíches devorados pelo mesmo Chestnut em 2011.
+O restaurante Nathan’s Famous Corporation, localizado no Brooklyn, NY, é o responsável pela competição. Eles produzem deliciosos cachorros-quentes, mundialmente famosos, mas quando o assunto é matemática eles não são tão bons. Eles desejam ser listados no Livro de Recordes do Guinness, mas para isso devem preencher um formulário descrevendo os fatos básicos da competição. Em particular, eles devem informar o número médio de cachorros-quentes consumidos pelos participantes durante a competição.
+Você pode ajudá-los? Eles prometeram pagá-lo com um dos seus saborosos cachorros-quentes. Dados o número total de cachorros-quentes consumidos e o número total de participantes na competição, você deve escrever um programa para determinar o número médio de cachorros-quentes consumidos pelos participantes.
+
+~~~~ENTRADA:
+A entrada consiste de uma única linha que contém dois inteiros H e P (1 ≤ H, P ≤ 1000) indicando respectivamente o número total de cachorros-quentes consumidos e o número total de participantes na competição.
+
+~~~~SAÍDA:
+Seu programa deve produzir uma única linha com um número racional representando o número médio de cachorros-quentes consumidos pelos participantes. O resultado deve ser escrito como um número racional com exatamente dois dígitos após o ponto decimal, arredondado se necessário.
+
+--------------------------------------
+Exemplo de Entrada | Exemplo de Saída
+--------------------------------------
+10 90              | 0.11
+--------------------------------------
+840 11             | 76.36
+--------------------------------------
+1 50               | 0.02
+--------------------------------------
+34 1000            | 0.03
+--------------------------------------
+35 1000            | 0.04
+--------------------------------------
+'''
+
+# DICAS SOBRE PYTHON:
+# FUNÇÃO input(): Ela recebe como parâmetro uma String que será visível ao usuário, 
+# onde geralmente informa que tipo de informação ele está esperando receber;
+# FUNÇÃO print(): Ela é a responsável por imprimir os dados e informar os valores no terminal;
+# MÉTODO split(): permite dividir o conteúdo da variável de acordo com as condições especificadas 
+# em cada parâmetro da função ou com os valores predefinidos por padrão;
+
+# Abaixo segue um exemplo de código que você pode ou não utilizar
+valores = input().split() 
+
+# TODO:  Calcule a média de cachorros quentes consumidas por participante e imprima o valor com DUAS casas decimais.
+hd_consumidos = int(valores[0])
+participantes = int(valores[1])
+media_consumo = ( hd_consumidos / participantes )
+
+print(f'{media_consumo:.2f}')
