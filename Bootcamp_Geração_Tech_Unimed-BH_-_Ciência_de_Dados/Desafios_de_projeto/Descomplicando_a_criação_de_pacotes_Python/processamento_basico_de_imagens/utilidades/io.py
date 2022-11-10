f6567b82e2694402af63d6e41ce19b16eb592c22
Joadir Damasceno Junior <115026650+TMKaT@users.noreply.github.com>
2022-11-10 12:36:21 -0300

    Delete Bootcamp_Geração_Tech_Unimed-BH_-_Ciência_de_Dados/Desafios_de_projeto/Descomplicando_a_criação_de_pacotes_Python directory

--- Bootcamp_Geração_Tech_Unimed-BH_-_Ciência_de_Dados/Desafios_de_projeto/Descomplicando_a_criação_de_pacotes_Python/processamento_basico_de_imagens/utilidades/io.py
+++
@@ -1,8 +0,0 @@
-from skimage.io import inread, insave
-
-def ler_imagem(caminho, is_gray = False):
-    img = inread(caminho, as_gray = is_gray)
-    return img
-
-def savar_imagem(img, caminho):
-    insave(caminho, img)
